1460e94436ef30d47b441dce0832a43b46ce9bf1
Amit Kaushik <kaushikabhi999@gmail.com>
2021-05-22 15:14:30 +0530

    transformed title

--- assets/js/day-night.js
+++ assets/js/day-night.js
@@ -15,43 +15,20 @@ let switchTime = () => {
   back.setAttribute('href', '#' + (isDay ? 'day' : 'night'));
   front.setAttribute('href', '#' + (isDay ? 'night' : 'day'));
   github.setAttribute('src', isDay ? githubWhite : githubBlack);
-  if (isDay) {
-    title.classList.remove('title-white')
-  } else {
-    title.classList.add('title-white')
-  }
 }
 let scale = 30;
 let toNightAnimation = gsap.timeline();
 
 toNightAnimation
   .to('#night-content', { duration: duration * 0.5, opacity: 1, ease: 'power2.inOut', x: 0 })
-  .to('#circle', {
-    duration: duration,
-    ease: 'power4.in',
-    scaleX: scale,
-    scaleY: scale,
-    x: 1,
-    transformOrigin: '100% 50%',
-  }, 0)
+  .to('#circle', { duration: duration, ease: 'power4.in', scaleX: scale, scaleY: scale, x: 1, transformOrigin: '100% 50%', }, 0)
   .to('.day-label', { duration: duration * 2, ease: 'power2.inOut', opacity: 0.2 }, 0)
   .to('.night-label', { duration: duration * 2, ease: 'power2.inOut', opacity: 1 }, 0)
-  .set('#circle', {
-    // transformOrigin: '0% 50%',
-    scaleX: -scale,
-    // x: 8.5,
-    onUpdate: () => switchTime()
-  }, duration).to('#circle', {
-    duration: duration,
-    ease: 'power4.out',
-    scaleX: -1,
-    scaleY: 1,
-    x: 2,
-  }, duration)
+  .set('#circle', { scaleX: -scale, onUpdate: () => switchTime() }, duration)
+  .to('#circle', { duration: duration, ease: 'power4.out', scaleX: -1, scaleY: 1, x: 2, }, duration)
   .to('#day-content', { duration: duration * 0.5, opacity: 0.5 }, duration * 1.5)
   .to('body', { backgroundColor: '#000', duration: duration * 2 }, 0)
-
-let stars = Array.from(document.getElementsByClassName('star'));
+  .to('#title', { color: '#fff', duration: duration * 2 }, 0)
 
 let switchToggle = document.getElementById('input');
 switchToggle.addEventListener('change', () => toggle())
@@ -64,9 +41,6 @@ let toggle = () => {
     toNightAnimation.play();
   }
 }
-
-
-
 
 toNightAnimation.reverse();
 toNightAnimation.pause();
